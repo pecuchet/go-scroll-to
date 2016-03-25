5308847b2efe83413ea57df8d0d888b32684d9e9
Arnaud <hello@dotburo.com>
2016-03-25 18:39:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# goScroll
+A fast and lightweight native javascript module to smoothly scrollTo. It scrolls by speed instead of duration, to a position or an element, within the window or a scrollable parent element. It's stoppable. Aside from a modern browser, it's dependency free.
+
+### Features
+
+- Scroll the window or any scrollable element
+- Scroll by speed: the duration is relative to the distance
+- Stop when the user manually scrolls (i.e. 'mouse wheels' or 'touch starts')
+- Sets a 'auto-scrolling' class on the body element, so you can disable hovers through CSS while scrolling.
+- Scroll to top or a specific position
+- Scroll to an element
+- Offset the target position
+- Touch, AMD and commonJS compatible
+
+### Support
+- IE9 needs a requestAnimationFrame polyfill
+
+## Examples
+### Scroll to top
+```
+goScroll();
+```
+
+### Scroll to position inside a scrollable element
+```
+goScroll({
+    to: 300,
+    context: document.getElementById('scrollable')
+    });
+```
+
+### Scroll slowly to element
+```
+goScroll({
+    to: document.getElementById('element'),
+    speed: .3
+    });
+```
+
+## Install through npm
+```
+npm install go-scroll-to
+```
+
+## All Options
+
+**to**: HTMLElement or Number. Default: 0  
+**offset**: Number, offset the target position. Default: 0  
+**context**: HTMLElement. Default: window  
+**speed**: Float greater than 0, higher is faster. Default: 2  
+**callback**: Function, triggered when target is reached or when the user interrupts the animation. The callback receives one argument, which will be `1` if the target position is reached. Default: undefined
+
+## License
+Apache 2.0
---
+++ goScroll.js
@@ -0,0 +1,94 @@
+(function ( w, d, undefined ) {
+    'use strict';
+
+    var CLASSNAME = 'auto-scrolling',   // className for body element while scrolling
+        SPEED     = 2,                  // default speed
+
+        scrolling = 0,                  // are-we-scrolling flag
+        css_regex = new RegExp( '(?:^|\\s)'+CLASSNAME+'(?!\\S)', 'gi' ),    // to remove the body className after scrolling
+
+        // if we are using a touch screen use touch start,
+        // else find a wheel event to stop the animation.
+        stopEvent = 'ontouchstart' in w || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0
+                ? 'touchstart'
+                : 'onwheel' in d.createElement( 'div' )
+                    ? 'wheel'                           // ie9+, sf7.0+, ff17.0+, ch31
+                    : d.onmousewheel !== undefined
+                        ? 'mousewheel'                  // ie6+, sf3.0+, ch1.0+
+                        : 'DOMMouseScroll';
+
+    // easing function
+    function easeOutQuad ( t, b, c, d ) {
+        return Math.round( -c *(t/=d)*(t-2) + b );
+    }
+
+    // find the position on an element
+    function position ( el ) {
+        var top = 0;
+        if ( el.offsetParent ) {
+            do {
+                top += el.offsetTop;
+            } while ( el = el.offsetParent )
+        }
+        return top;
+    }
+
+    // set are-we-scrolling flag stopEvent handler
+    function listen() { scrolling = 0; }
+
+    function go ( options ) {
+        options = options || {};
+
+        var context = options.context || w,// default context is the window, else a given element
+            targetPos = options.to && options.to.nodeType === 1 ? position(options.to) : (options.to || 0),
+            startPos = context[ options.context ? 'scrollTop' : 'pageYOffset' ],
+            distance = (targetPos - (options.offset || 0)) - startPos,
+            startTime = +new Date(),
+            duration = Math.abs( distance / (options.speed || SPEED) ),
+            targetTime = startTime + duration,
+            callback = options.callback,
+            handleScroll = options.context
+                ? function ( y ) { context.scrollTop = y; }
+                : function ( y ) { context.scrollTo( 0, y ); };
+
+        function scroll () {
+            var now = +new Date();
+
+            // scroll for some time
+            if ( scrolling && now <= targetTime ) {
+                handleScroll( easeOutQuad( now - startTime, startPos, distance, duration ) );
+                w.requestAnimationFrame( scroll );
+                return;
+            }
+
+            // time's up
+            // if the animation is still running,
+            // be certain we reached our target position.
+            if ( scrolling ) {
+                w.requestAnimationFrame(function() { handleScroll(targetPos); });
+            }
+            // callback
+            if ( typeof callback === "function" ) {
+                callback.call( context, scrolling );
+            }
+            // remove the stop event handler
+            context.removeEventListener( stopEvent, listen );
+            // remove the body class
+            d.body.className = d.body.className.replace( css_regex, '' );
+        }
+
+        scrolling = 1;                                  // wave the flag
+        d.body.className += ' ' + CLASSNAME;            // we-are-scrolling className
+        context.addEventListener( stopEvent, listen );  // let the user stop the animation
+        w.requestAnimationFrame( scroll );              // go!
+    }
+
+    // UMD
+    if (typeof define === 'function' && define.amd) {
+        define([], function(){ return go; });
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = go;
+    } else {
+        window.goScroll = go;
+    }
+} ( window, document ) );
